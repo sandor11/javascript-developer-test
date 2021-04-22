54c71037e170adb93b9ddeeb7ec76379595f4a63
Sandy Agafonoff <sandy@agafonoff.dev>
2021-04-22 12:01:04 +1000

    simplest pass test case with hard coded values

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -2,7 +2,13 @@ const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
-  // return results;
+  const results = [
+    { 'Arnie Quote': 'Get to the chopper' },
+    { 'Arnie Quote': 'MY NAME IS NOT QUAID' },
+    { 'Arnie Quote': `What's wrong with Wolfie?` },
+    { 'FAILURE': 'Your request has been terminated' }
+  ]
+  return results;
 };
 
 module.exports = {
