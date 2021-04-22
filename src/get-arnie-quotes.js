94c8e82490a34a0de08b1f0da1d23281eaf55b69
Sandy Agafonoff <sandy@agafonoff.dev>
2021-04-22 12:28:57 +1000

    execute each http call in parallel to meet performance requirement

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,26 +1,40 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  const results = [];
-  for (url of urls) {
-    const result = await httpGet(url);
-    results.push(createResponse(result.status, result.body));
-  }
-  return results;
+  const httpCalls = urls.map(
+    (url) =>
+      new Promise(async (resolve, reject) => {
+        try {
+          const result = await httpGet(url);
+          resolve(createResponse(result.status, result.body));
+        } catch (err) {
+          reject(err);
+        }
+      })
+  );
+
+  return new Promise(async function (resolve, reject) {
+    Promise.all(httpCalls)
+      .then((values) => {
+        resolve(values);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 const responseKeys = {
-  200: 'Arnie Quote',
-  500: 'FAILURE'
-}
+  200: "Arnie Quote",
+  500: "FAILURE",
+};
 
 const createResponse = (status, stringified) => {
   const parsed = JSON.parse(stringified);
   return {
-    [responseKeys[status]]: parsed.message
-  }
-}
+    [responseKeys[status]]: parsed.message,
+  };
+};
 
 module.exports = {
   getArnieQuotes,
